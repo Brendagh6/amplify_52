6ff7d0f6930d5e2bdcdbcdf71246f4bfeee0f9ee
Brendagh6 <bh4331265@gmail.com>
2025-02-19 23:49:14 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+      <p>web dev</p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -18,7 +19,7 @@ function App() {
           Learn React
         </a>
       </header>
-            <p>web dev</p>
+          
     </div>
   );
 }
